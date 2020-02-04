064082a9d2d523b1248ed19f09cccfdc1dcd5872
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-02-04 09:52:49 -0600

    Calculando todos los años bisiestos hasta el 2020

--- Practica3/programa2_2.cpp
+++ Practica3/programa2_2.cpp
@@ -3,11 +3,21 @@
 
 int main(){
 	Fecha a, b, c(21, 9 ,1973),d(1,4,2014);
+	int total=0;
 	b.inicializaFecha(17, 6, 2000);
 	a.muestraFecha();
 	b.muestraFecha();
 	c.muestraFecha();
 	d.convierte();
 	cout << " Es bisiesto :" << a.leapyr() << endl;
+
+	Fecha e;
+	for ( int i = 1; i < 2021 ; i++){
+		e.inicializaFecha(1,1,i);
+		if ( e.leapyr() )
+			total++;
+	}
+	cout << "Total de años bisiestos : " << total << endl;
+
 	return 0;
 }
