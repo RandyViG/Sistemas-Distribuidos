a8551c1aaf03528ad5194249e87402515f650394
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-04-01 21:22:24 -0600

    Usando memcpy para recuperar el valor de retorno de doOperation

--- Practica9/TimeOut/cliente.cpp
+++ Practica9/TimeOut/cliente.cpp
@@ -8,14 +8,14 @@ int main(int argc, char* argv[]){
 		printf("Uso: %s IP del servidor Val1 Val2 intentos\n", argv[0]);
 		exit(1);
 	}
-	char num[2] , *resultado;
+	char num[2] , resultado;
     int ptoServidor = 7300;
     num[0] = atoi(argv[2]);
     num[1] = atoi(argv[3]);
     Solicitud sol;
     for (int i = 0; i < atoi(argv[4]) ; i++){
-        resultado = sol.doOperation( argv[1] , ptoServidor , 1 , num );
-        printf("\nRespuesta: %d\n",resultado[0]);
+        memcpy(&resultado,sol.doOperation( argv[1] , ptoServidor , 1 , num ),1);
+        printf("\nRespuesta: %d\n",resultado);
     }
     
     return 0;
