16e121c9e7c69f27b0b375e6f9d6b11e5c40a6ea
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-05-13 21:42:42 -0500

    Agregando Distribución de procesamiento

---
+++ DistribucionProcesamiento/servidor.cpp
@@ -0,0 +1,57 @@
+#include "Respuesta.h"
+#include "registro.h"
+#include "trie.h"
+#include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+#include <sys/time.h>
+
+using namespace std;
+
+int main( int argc , char *argv[] ){
+    if( argc != 3 ){
+        printf("Uso: %s pto Nombre_Archivo \n" , argv[0]);
+        exit(0);
+    }
+    int pto = atoi(argv[1]);
+    Respuesta res( pto );
+    struct mensaje datos;
+    struct registro reg;
+    struct timeval timestamp;
+    struct TrieNode *trie = getNode();
+
+    int archivo;
+    string aux;
+
+    printf("\n********** Servidor iniciado **********");
+    printf("\nEsperando mensajes en el puerto: %d\n",pto);
+    
+    while(1) {
+        memcpy( &datos,res.getRequest( ),sizeof(datos) );
+        if( res.getError() == 0 ){
+            gettimeofday( &timestamp , NULL );
+            memcpy( &reg , datos.arguments , sizeof(datos.arguments) );
+            if ( !search( trie , string(reg.celular) ) ){
+                insert( trie , string(reg.celular) );
+                printf("Registro exitoso  \nCel: %s\n",reg.celular);
+                printf("CURP: %s\n",reg.CURP);
+                printf("Partido: %s\n\n",reg.partido);
+                aux = reg.toString() + " " 
+                    + to_string(timestamp.tv_sec) + "-" 
+                    + to_string(timestamp.tv_usec) +  "\n";
+                if( ( archivo = open(argv[2], O_WRONLY |O_CREAT|O_APPEND,0666) ) == -1 )
+                    perror(argv[2]);
+	            write( archivo , aux.c_str() , aux.length() );
+	            fsync( archivo );
+                close( archivo );
+            }
+            else{
+                timestamp.tv_sec = 0;
+                timestamp.tv_usec = 0;
+            }
+        }
+        res.sendReply( (char*)&timestamp );
+    }
+}
